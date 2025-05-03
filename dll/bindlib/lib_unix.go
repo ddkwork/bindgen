1f6b4cb3f092d027f2136acee37559f99820498c
Admin <2762713521@qq.com>
2025-05-03 18:12:13 +0800

    dev

--- dll/bindlib/lib_unix.go
+++ dll/bindlib/lib_unix.go
@@ -6,7 +6,6 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"os"
-	"strings"
 
 	"github.com/ddkwork/golibrary/mylog"
 	"github.com/ebitengine/purego"
@@ -17,43 +16,42 @@ type solib struct {
 }
 
 func (w solib) Lookup(name string) uintptr {
-	addr := mylog.Check2(purego.Dlsym(w.Handle, name))
-	return addr
+	return mylog.Check2(purego.Dlsym(w.Handle, name))
 }
 
 func LoadLibrary(name string) (LoadedLibrary, error) {
 	h := mylog.Check2(purego.Dlopen(name, purego.RTLD_NOW|purego.RTLD_LOCAL))
-	if err == nil {
-		return solib{Handle: h}, nil
-	}
-	if !strings.ContainsAny(name, "/\\") {
-		h, elocal := purego.Dlopen("./"+name, purego.RTLD_NOW|purego.RTLD_LOCAL)
-		if elocal == nil {
-			return solib{Handle: h}, nil
-		}
-	}
-	return nil, err
+	// if err == nil {
+	return solib{Handle: h}, nil
+	// }
+	// if !strings.ContainsAny(name, "/\\") {
+	// 	h, elocal := purego.Dlopen("./"+name, purego.RTLD_NOW|purego.RTLD_LOCAL)
+	// 	if elocal == nil {
+	// 		return solib{Handle: h}, nil
+	// 	}
+	// }
+	// return nil, err
 }
 
 func FindLibrary(name string) (LoadedLibrary, error) {
 	lib := mylog.Check2(LoadLibrary(name))
-	if err == nil {
-		return lib, nil
-	}
-	org := err
-	if !strings.HasSuffix(name, ".so") {
-		name += ".so"
-		if lib = mylog.Check2(LoadLibrary(name)); err == nil {
-			return lib, nil
-		}
-	}
-	if !strings.HasPrefix(name, "lib") {
-		name = "lib" + name
-		if lib = mylog.Check2(LoadLibrary(name)); err == nil {
-			return lib, nil
-		}
-	}
-	return nil, org
+	// if err == nil {
+	return lib, nil
+	// }
+	// org := err
+	// if !strings.HasSuffix(name, ".so") {
+	// 	name += ".so"
+	// 	if lib = mylog.Check2(LoadLibrary(name)); err == nil {
+	// 		return lib, nil
+	// 	}
+	// }
+	// if !strings.HasPrefix(name, "lib") {
+	// 	name = "lib" + name
+	// 	if lib = mylog.Check2(LoadLibrary(name)); err == nil {
+	// 		return lib, nil
+	// 	}
+	// }
+	// return nil, org
 }
 
 func LoadLibraryEmbed(data []byte) (LoadedLibrary, error) {
